c9ff22a0d6f8cec3b42c8764bcdde31ea9576865
Dominik Hübner <dominikhubner@protonmail.com>
2021-04-20 00:06:06 +0200

    source inputString function

--- readability.c
+++ readability.c
@@ -90,7 +90,7 @@ float count_sentences(char *text)
     return sentences;
 }
 
-char *inputString(FILE* fp, size_t size){
+char *inputString(FILE* fp, size_t size){   //source: https://stackoverflow.com/a/16871702/12313613
     char *str;
     int ch;
     size_t len = 0;
